7cf7474fc744229d4cf19f44649dacf85ce0b6ab
Jean-Baptiste Jouband <jean-baptiste@jouband.com>
2021-10-02 17:01:25 -0600

    Center permission text

--- App.tsx
+++ App.tsx
@@ -26,10 +26,18 @@ export default function App() {
   };
 
   if (hasPermission === null) {
-    return (<Text>Requesting for camera permission</Text>);
+    return (
+      <View style={styles.container}>
+	<Text>Requesting for camera permission</Text>
+      </View>
+    );
   }
   if (hasPermission === false) {
-    return (<Text>No access to camera</Text>);
+    return (
+      <View style={styles.container}>
+	<Text>No access to camera</Text>
+      </View>
+    );
   }
 
   return (
