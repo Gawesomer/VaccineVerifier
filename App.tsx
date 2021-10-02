90cc2ebfdfa91a95022f1487d99e86d5fdb618d5
Jean-Baptiste Jouband <jean-baptiste@jouband.com>
2021-10-02 16:57:44 -0600

    Basic QR code scanner

--- App.tsx
+++ App.tsx
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);  // Only run effect once on mount.
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    Alert.alert(
+      'Successful scan',
+      `Bar code with type ${type} and data ${data} has been scanned!`,
+      [
+	{ text: 'OK', onPress: () => setScanned(false) },
+      ]
+    );
+  };
+
+  if (hasPermission === null) {
+    return (<Text>Requesting for camera permission</Text>);
+  }
+  if (hasPermission === false) {
+    return (<Text>No access to camera</Text>);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello, world!</Text>
+      <BarCodeScanner
+	onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+	style={StyleSheet.absoluteFillObject}
+      />
       <StatusBar style="auto" />
     </View>
   );
