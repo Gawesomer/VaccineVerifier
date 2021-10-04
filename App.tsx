dadf738c0af6877619d69d7f76b71583bd9c9aeb
Jean-Baptiste Jouband <jean-baptiste@jouband.com>
2021-10-03 20:48:48 -0600

    Validate JWS

--- App.tsx
+++ App.tsx
@@ -3,7 +3,49 @@ import React, { useEffect, useState } from 'react';
 import { Alert, StyleSheet, Text, View } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
+function shcTob64(shc: string): string {
+  /* args:
+     shc - e.g. 'shc:/1234<and-so-on>'
+
+     returns:
+     b64 url encoded jws string
+  */
+  shc = shc.slice(5);
+  let b64digits = [];
+  for (let i = 0; i < shc.length; i+=2) {
+    b64digits.push(parseInt(shc[i])*10 + parseInt(shc[i+1]) + 45);
+  }
+  return b64digits.map((c) => String.fromCharCode(c)).join('');
+}
+
+function isValidJWS(keystore: KeyStore, jws: string): boolean {
+  let result = false;
+  jose.JWS.createVerify(keystore).verify(jws).then(function(result) {result=true;}).catch((error) => {result = false;});
+  return result;
+}
+
 export default function App() {
+  const JWKS_SK = {  // https://skphr.prd.telushealthspace.com/.well-known/jwks.json
+    keys: [
+      {
+	kty: 'EC',
+	use: 'sig',
+	crv: 'P-256',
+	kid: 'xOqUO82bEz8APn_5wohZZvSK4Ui6pqWdSAv5BEhkes0',
+	x: 'Hk4ktlNfoIIo7jp5I8cefp54Ils3TsKvKXw_E9CGIPE',
+	y: '7hVieFGuHJeaNRCxVgKeVpoxDJevytgoCxqVZ6cfcdk',
+	alg: 'ES256'
+      }
+    ]
+  };
+
+  let keystore = undefined;
+  jose.JWK.asKeyStore(jwks).
+    then(function(result) {
+      // {result} is a jose.JWK.KeyStore
+      keystore = result;
+    });
+
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(null);
 
@@ -15,9 +57,10 @@ export default function App() {
   }, []);  // Only run effect once on mount.
 
   const handleBarCodeScanned = ({ type, data }) => {
+    const isSuccess = isValidJWS(keystore, shcTob64(data));
     setScanned(true);
     Alert.alert(
-      'Successful scan',
+      isSuccess ? 'Scan succeeded' : 'Scan failed',
       `Bar code with type ${type} and data ${data} has been scanned!`,
       [
 	{ text: 'OK', onPress: () => setScanned(false) },
